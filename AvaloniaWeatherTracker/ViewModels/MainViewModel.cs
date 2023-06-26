4168cc6773fa892c3e807bafdf17672fc38bf501
Leo <77863733+leofox2021@users.noreply.github.com>
2023-06-27 02:13:11 +0300

    add right flyout panel with animation

--- AvaloniaWeatherTracker/ViewModels/MainViewModel.cs
+++ AvaloniaWeatherTracker/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
+using Avalonia;
 using AvaloniaWeatherTracker.Commands;
 using AvaloniaWeatherTracker.Models;
 using AvaloniaWeatherTracker.Services;
@@ -12,6 +13,9 @@ public partial class MainViewModel : ObservableObject
     [ObservableProperty] 
     private ObservableCollection<WeatherReport> _weatherReports;
 
+    [ObservableProperty] 
+    private Thickness _flyoutMargin; 
+    
     [ObservableProperty]
     private string _status;
 
@@ -20,6 +24,17 @@ public partial class MainViewModel : ObservableObject
 
     [ObservableProperty]
     private int _selectedIndex;
+
+    [ObservableProperty]
+    private double _shadowRectangleOpacity;
+
+    [ObservableProperty]
+    private bool _isShadowRectangleEnabled;
+
+    private const double FlyoutClosedMargin = 700;
+    private const double FlyoutOpenMargin = 250;
+    private const double FlyoutClosedOpacity = 0;
+    private const double FlyoutOpenOpacity = 0.6;
     
     public MainViewModel()
     {
@@ -28,14 +43,21 @@ public partial class MainViewModel : ObservableObject
         LoadReports = new RelayCommand(DoLoadWeatherReports, CanDoLoadWeatherReports);
         AddCity = new RelayCommand(DoAddCity, CanDoAddCity);
         RemoveCity = new RelayCommand(DoRemoveCity, CanDoRemoveCity);
+        ShowWeatherFlyout = new RelayCommand(DoShowWeatherFlyout, CanShowWeatherFlyout);
+        HideWeatherFlyout = new RelayCommand(DoHideWeatherFlyout, CanHideWeatherFlyout);
         
         _status = "Last fetched at: never fetched!";
         _textBotText = "";
+        _flyoutMargin = new Thickness(FlyoutClosedMargin, 0, 0, 0);
+        _shadowRectangleOpacity = FlyoutClosedOpacity;
+        _isShadowRectangleEnabled = false;
     }
 
     public ICommand LoadReports { get; set; }
     public ICommand AddCity { get; set; }
     public ICommand RemoveCity { get; set; }
+    public ICommand ShowWeatherFlyout { get; set; }  
+    public ICommand HideWeatherFlyout { get; set; }
     
     private bool CanDoLoadWeatherReports(object obj) => true;
 
@@ -59,5 +81,23 @@ public partial class MainViewModel : ObservableObject
     {
         await WeatherService.RemoveCity(WeatherReports, SelectedIndex);
         Status = WeatherService.Status;
+    }
+
+    private bool CanShowWeatherFlyout(object obj) => true;
+ 
+    private void DoShowWeatherFlyout(object obj)
+    {
+        IsShadowRectangleEnabled = true;
+        ShadowRectangleOpacity = FlyoutOpenOpacity;
+        FlyoutMargin = new Thickness(FlyoutOpenMargin, 0, 0, 0);
+    }
+    
+    private bool CanHideWeatherFlyout(object obj) => true;
+
+    private void DoHideWeatherFlyout(object obj)
+    {
+        IsShadowRectangleEnabled = false;
+        ShadowRectangleOpacity = FlyoutClosedOpacity;
+        FlyoutMargin = new Thickness(FlyoutClosedMargin, 0, 0, 0);
     }
 }
