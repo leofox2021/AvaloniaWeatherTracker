765e466e7b1b16b2bae6cb9c001fcc6e9af3afdb
Leo <77863733+leofox2021@users.noreply.github.com>
2023-07-08 01:22:46 +0400

    fix resources typo

---
+++ AvaloniaWeatherTracker/Services/WeatherImagePicker.cs
@@ -0,0 +1,11 @@
+﻿using AvaloniaWeatherTracker.Models;
+
+namespace AvaloniaWeatherTracker.Services;
+
+// public static class WeatherImagePicker
+// {
+//     public static string Pick(WeatherReport weatherReport) => weatherReport.DegreesCelsius switch
+//     {
+//         > 25 => "../Resuorces/wind_speed.png" 
+//     }
+// }
