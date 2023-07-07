d4953bb4104fd3178202bdb4a4e1e30a39b4ae07
Leo <77863733+leofox2021@users.noreply.github.com>
2023-07-07 20:23:02 +0400

    update to v11 release

--- AvaloniaWeatherTracker/Models/ExtendedWeatherRecord.cs
+++ AvaloniaWeatherTracker/Models/ExtendedWeatherRecord.cs
@@ -16,4 +16,11 @@ public class ExtendedWeatherReport
     public float Elevation { get; set; }
     public int Population { get; set; }
     public string? CountryCode { get; set; }
+    public float RainSum { get; set; }
+    public float Precipitation { get; set; }
+    public float Showers { get; set; }
+    public float Snowfall { get; set; }
+    public string? Sunset { get; set; }
+    public string? Sunrise { get; set; }
+    public float WindDirection { get; set; }
 }
